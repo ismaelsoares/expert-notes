5cb225f2afa67763698dc6cc24a82889426f4116
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-02-08 19:22:45 -0300

    Add radix-ui and modals

--- src/app.tsx
+++ src/app.tsx
@@ -2,6 +2,11 @@ import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+const note = {
+  date: new Date(),
+  content: 'Hello note 01'
+}
+
 export function App() {
   return (
     <div className='max-w-6xl mx-auto my-12 space-y-6'>
@@ -21,13 +26,7 @@ export function App() {
 
         <NewNoteCard />
 
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-        <NoteCard />
-
+        <NoteCard note={note} />
 
       </div>
     </div>
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,14 +1,46 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+interface NoteCardProps {
+    note: {
+        date: Date
+        content: string
+    }
+}
 
-export function NoteCard() {
+
+export function NoteCard({ note }: NoteCardProps) {
     return (
-        <button className="relative p-5 space-y-3 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
-            <span className="text-sm font-medium text-slate-200">
-                Há 4 dias
-            </span>
-            <p className='text-sm leading-6 text-slate-400'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint laboriosam quia architecto, eos dolorum ea maxime facilis necessitatibus ad aliquam placeat beatae velit officiis nostrum repudiandae in voluptates quo amet.
-            </p>
-            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
-        </button>
+        <Dialog.Root>
+
+            <Dialog.Trigger className="relative flex flex-col gap-3 p-5 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
+                <span className="text-sm font-medium text-slate-200">
+                    {note.date.toISOString()}
+                </span>
+                <p className='text-sm leading-6 text-slate-400'>
+                    {note.content}
+                </p>
+                <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
+            </Dialog.Trigger>
+            <Dialog.Portal>
+                <Dialog.Overlay className='fixed inset-0 bg-black/50' />
+                <Dialog.Content className='fixed -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none'>
+                    <div className="flex flex-col flex-1 gap-3 p-5">
+                        <span className="text-sm font-medium text-slate-200">
+                            {formatDistanceToNow(note.date, { locale: ptBR, addSuffix: true })}
+                        </span>
+                        <p className='text-sm leading-6 text-slate-400'>
+                            {note.content}
+                        </p>
+                    </div>
+                    <button
+                        type='button'
+                        className='w-full py-4 text-sm text-center bg-slate-800 text-slate-300 outline-now'
+                    >
+                        Deseja apagar essa nota?
+                    </button>
+                </Dialog.Content>
+            </Dialog.Portal>
+        </Dialog.Root>
     )
 }
