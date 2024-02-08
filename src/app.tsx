1c3e784a5cf247f24ec5db9f80ebaae571d0b9f5
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-02-07 22:50:48 -0300

    Add hover focus bg lime 400

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,35 @@
 import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
 
 export function App() {
   return (
-    <div className='mx-auto'>
-      <img src={logo} alt="logo" />
-      <input type="text" placeholder='Busque em suas notas' />
+    <div className='max-w-6xl mx-auto my-12 space-y-6'>
+      <img src={logo} alt="NLV expert" />
+
+      <form className='w-full mt-6'>
+        <input
+          type="text"
+          placeholder='Busque em suas notas'
+          className='w-full font-semibold tracking-tight bg-transparent outline-none text-3x1 placeholder:text-slate-500'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700' />
+
+      <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
+
+        <NewNoteCard />
+
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+        <NoteCard />
+
+
+      </div>
     </div>
   )
 }
