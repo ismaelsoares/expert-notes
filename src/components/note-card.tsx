a2cf0c7a975147e0d2319a06df6b720cf5399b70
Ismael Soares Doria <ismaelsoaresdoria@gmail.com>
2024-02-07 22:42:48 -0300

    Add hover focus lime 400

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -1,7 +1,7 @@
 
 export function NoteCard() {
     return (
-        <div className="relative p-5 space-y-3 overflow-hidden rounded-md pointer-events-none bg-slate-800">
+        <button className="relative p-5 space-y-3 overflow-hidden text-left rounded-md outline-none bg-slate-800 hover:ring-2 hover:ring-slate-600 focus:ring-2 focus:ring-lime-400">
             <span className="text-sm font-medium text-slate-200">
                 Há 4 dias
             </span>
@@ -9,6 +9,6 @@ export function NoteCard() {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint laboriosam quia architecto, eos dolorum ea maxime facilis necessitatibus ad aliquam placeat beatae velit officiis nostrum repudiandae in voluptates quo amet.
             </p>
             <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/60 to-black/0 h-1/2" />
-        </div>
+        </button>
     )
 }
